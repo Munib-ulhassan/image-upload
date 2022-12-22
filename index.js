4080eecb70d8b91a62f169683f2f87b0ded58f7d
Munib-ulhassan <119694678+Munib-ulhassan@users.noreply.github.com>
2022-12-22 19:41:29 +0500

    image uploa dapi done

---
+++ index.js
@@ -0,0 +1,50 @@
+const multer = require('multer');
+const express = require('express');
+
+const app = express();
+var cors = require('cors');
+app.use(cors());
+
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        // console.log(file)
+        // console.log(req)
+
+        // const isValid = FILE_TYPE_MAP[file.mimetype];
+        // let uploadError = new Error('invalid voice type');
+
+        // if (isValid) {
+        //     uploadError = null;
+        // }
+        cb(null, 'uploads');
+    },
+    filename: function (req, file, cb) {
+        console.log(file)
+        const fileName = file.originalname.split(' ').join('-');
+        cb(null, `${fileName}`);
+    }
+});
+const upload = multer({ storage: storage });
+
+app.post('/image',upload.array('file'), (req, res) => {
+    const files = req.files;
+    
+    const basePath = `http://${req.get('host')}/uploads/`;
+    let imagePaths = [];
+
+    if (files) {
+        files.map((file) => {
+            imagePaths.push(`${basePath}${file.filename}`);
+
+        });
+    }
+    console.log(imagePaths)
+    res.status(200).json({status:1,imagePaths})
+})
+
+app.listen(3030, () => {
+    console.log('server is running on 4021 port')
+ 
+})
